89d087876de403aae09e29351486eaefd4f3af07
Alexanyer Naranjo <alexanyernaranjo@gmail.com>
2026-05-30 13:49:28 -0400

    update: Contact

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -28,7 +28,7 @@ const sections = [
   {
     icon: '📞',
     title: 'Contacto',
-    content: '0412-028-3147 / 0412-013-7644',
+    content: '0412-028-3147',
     action: { label: 'WhatsApp', url: 'https://wa.me/584120283147' },
   },
 ]
